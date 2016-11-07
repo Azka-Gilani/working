710f72259bebfa56f67efe6ee64b289b0a9e2e8a
Azka-Gilani <azka.gilani@gmail.com>
2016-11-07 11:04:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ def makeWebhookResult(data):
     return {
         "speech": speech,
         "displayText": speech
-        #"originalRequest":{"source":"facebook","data": {"facebook": message}}
+        "originalRequest":{"source":"facebook","data": {"facebook": message}}
         #"data": {"facebook": message},
         # "contextOut": [],
         #"source": "apiai-weather-webhook-sample"
