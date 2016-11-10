477acb35451728454997ae3caed4088686e76263
Azka-Gilani <azka.gilani@gmail.com>
2016-11-10 17:14:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,13 @@ def processRequest(req):
         QR[3]="Price Range"
         QR[4]="Land Area"
         QR[5]="City?Specify"
+    elif "ChooseSector" in intent_name:        
+        QR[0]="(Y)"
+        QR[1]="Property Type"
+        QR[2]="Hot Property"
+        QR[3]="Price Range"
+        QR[4]="Land Area"
+        QR[5]="Other Sector?Specify"   
     
     city_names=processlocation(req)
     sector_names=processSector(req)
