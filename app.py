d976d4be97056a76ebb6a3d0fa9451c4dc016ef0
Azka-Gilani <azka.gilani@gmail.com>
2016-12-03 13:25:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -225,12 +225,19 @@ def makeWebhookResult(data):
         row_price[i]=data[i]['price']
         row_slug[i]=data[i]['slug']
         i+=1
-    global url
-    url = "http://www.aarz.pk/assets/images/properties/"+row_id[0]+"/"+row_id[0]+".actual.0.jpg"
+    global url1
+    global url2
+    url1 = "http://www.aarz.pk/assets/images/properties/"+row_id[0]+"/"+row_id[0]+".actual.0.jpg"
+    url2 = "http://www.aarz.pk/assets/images/properties/"+row_id[1]+"/"+row_id[1]+".actual.0.jpg"
     try:
-        urllib2.urlopen(url)
+        urllib2.urlopen(url1)
     except urllib2.HTTPError, e:
-        url= "http://www.aarz.pk/assets/images/properties/"+row_id[0]+"/"+row_id[0]+".actual.1.jpg"
+        url1= "http://www.aarz.pk/assets/images/properties/"+row_id[0]+"/"+row_id[0]+".actual.1.jpg"
+        
+        try:
+        urllib2.urlopen(url2)
+    except urllib2.HTTPError, e:
+        url2= "http://www.aarz.pk/assets/images/properties/"+row_id[0]+"/"+row_id[0]+".actual.1.jpg"
                 
     # print(json.dumps(item, indent=4))
     speech = "This is the response from server."+ row_title[0]+""+intent_name
@@ -259,7 +266,7 @@ def makeWebhookResult(data):
           {
              "title":row_title[0],
              "item_url":"http://www.aarz.pk/property-detail/"+row_slug[0],
-             "image_url": url,
+             "image_url": url1,
              "subtitle":row_location[0],
              "buttons":[
               {
@@ -315,7 +322,7 @@ def makeWebhookResult(data):
                "title": row_title[0],
                "subtitle": row_location[0],
                "item_url": "https://www.aarz.pk/property-detail/"+row_slug[0],               
-               "image_url": url ,
+               "image_url": url1 ,
                 "buttons": [{
                 "type": "web_url",
                 "url": "www.aarz.pk",
@@ -327,7 +334,7 @@ def makeWebhookResult(data):
                 "title": row_title[1],
                 "subtitle": row_location[1],
                 "item_url":  "http://www.aarz.pk/property-detail?id="+row_id[1],               
-                "image_url": url,
+                "image_url": url2,
                 "buttons": [{
                 "type": "web_url",
                 "url": "www.aarz.pk",
